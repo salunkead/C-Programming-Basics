4ba5d0ae1c43c086462e30e8b7db9285022b21f8
Amol Salunke <salunkeamol10@gmail.com>
2023-11-27 10:17:58 +0530

    The first c program

---
+++ 01_first_c_program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello,Github........!");
+    return 0;//indicate successful program execution
+}
+/*
+line number and it's Explaination:-
+line.1:- it is the standard input-output library,it contains printf,scanf,fopen,fclose functions
+         here we are telling compiler to include stdio.h file and i want to use the contents of the file
+line.2:- it is the starting point of execution,compiler starts executing program from main() function
+         int means it returns integer type after calling,the main function in c program is called by the operating system when the program is executed.
+         when you run a c program,the operating system is responsible for launching the program and as part of the program startup,it call's main() function. 
+line.3:- {} curly brace is used to group statements i.e all the statements defined inside curly brace belongs to main function
+line.4:- printf function is used to print message on console.this predefined function is available in stdio.h header file.
+line.5:- the return 0 statement is the last line of the main function,signaling to the operating system that the program executed without errors. 
+*/

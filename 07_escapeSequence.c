3c5b09a81b0113f632dff7035cba88511f3ff154
Amol Salunke <salunkeamol10@gmail.com>
2023-11-27 11:01:28 +0530

    Escape sequences in c

---
+++ 07_escapeSequence.c
@@ -0,0 +1,23 @@
+// escape sequence in c
+#include <stdio.h>
+void main()
+{
+    printf("Amol\nSalunke\n"); // new line
+    printf("----------------------\n");
+    printf("Amol\bSalunke\n"); // back space
+    printf("----------------------\n");
+    printf("Amol\tSalunke\n"); // tab horizontal
+    printf("----------------------\n");
+    printf("Amol\vSalunke\n"); // tab vertical
+}
+
+/*
+escape sequence in c programming is a sequence of characters
+escape sequence are useful for controlling the formatting of the output and including special characters in string.
+it is composed of two things backslash(\) and a character
+escapeSequence  meaning
+        \n       new line
+        \t       tab(horizontal)
+        \v       tab(vertical)
+        \b       backspace
+*/

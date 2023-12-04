6a2ce54c1db48f17eff3c8a8cd187e2f8e812217
Amol Salunke <salunkeamol10@gmail.com>
2023-12-04 11:09:40 +0530

    Preprocessor in c - #include

---
+++ 46_preprocessorInC1.c
@@ -0,0 +1,16 @@
+// Preprocessor in c:#include
+#include <stdio.h>
+float sum(float a, float b)
+{
+    float sum;
+    sum = a + b;
+    return sum;
+}
+
+/*
+1.preproccessor comes under the action before the actual compilation process.
+2.c preprocessor are not the part of the c compiler
+3.it is a textual substitution tool.
+4.All the proprocessor commands begin with #
+  ex: #include,#define,#if,#else,#undef,#ifdef etc
+*/
